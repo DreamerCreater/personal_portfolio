38528e69c7bc51f4ceb1a620849b9f271130ec99
Gerlee Chimedbat <GerleeC@riotinto.com>
2024-07-20 12:03:48 +0800

    removed unnecessary pictures

--- finance/urls.py
+++ finance/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name = "index"),
+    path("plan", views.index, name = "index"),
     path("<str:name>", views.greet, name = "greet"),
     
 ]
